52a822233b2e78cfffc1c5f94dce68a2311a593d
Javier1504 <114123536+Javier1504@users.noreply.github.com>
2023-11-21 12:50:14 +0700

    Add files via upload

---
+++ pendaftaran-mahasiswa/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Mahasiswa Baru | Javier Coding</title>
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Mahasiswa Baru</h3>
+        <h1>Javier Coding</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-mahasiswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+
+    </body>
+</html>
